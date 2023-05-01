db44bf9843ecf0294723b4edf499c0961f2226d3
KH <deinvictus98@gmail.com>
2023-05-01 17:09:01 +0800

    Code cleanup with new updates

--- web/src/App.js
+++ web/src/App.js
@@ -1,267 +1,49 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import * as tf from '@tensorflow/tfjs';
-
-// const App = () => {
-//   const [image, setImage] = useState(null);
-//   const [result, setResult] = useState('');
-
-//   const takePicture = async () => {
-//     const mediaDevices = navigator.mediaDevices;
-//     if (mediaDevices && mediaDevices.getUserMedia) {
-//       const stream = await mediaDevices.getUserMedia({ video: true });
-//       const video = document.querySelector('video');
-//       video.srcObject = stream;
-//       video.play();
-//     }
-//   };
-
-//   const uploadImage = (files) => {
-//     const file = files[0];
-//     const reader = new FileReader();
-//     reader.readAsDataURL(file);
-//     reader.onload = async () => {
-//       setImage(reader.result);
-//       const img = new Image();
-//       img.src = reader.result;
-//       img.onload = async () => {
-//         const tensor = tf.browser.fromPixels(img).resizeNearestNeighbor([224, 224]).toFloat();
-//         const offset = tf.scalar(255);
-//         const normalized = tensor.div(offset);
-//         const batched = normalized.expandDims(0);
-//         const formData = new FormData();
-//         formData.append('image', batched.arraySync());
-//         const response = await axios.post('http://localhost:5000/predict', formData);
-//         setResult(response.data);
-//       };
-//     };
-//   };
-
-//   const predict = async () => {
-//     setResult('Predicting...');
-//     const img = new Image();
-//     img.src = image;
-//     img.onload = async () => {
-//       const tensor = tf.browser.fromPixels(img).resizeNearestNeighbor([224, 224]).toFloat();
-//       const offset = tf.scalar(255);
-//       const normalized = tensor.div(offset);
-//       const batched = normalized.expandDims(0);
-//       const formData = new FormData();
-//       formData.append('image', batched.arraySync());
-//       const response = await axios.post('http://localhost:5000/predict', formData);
-//       setResult(response.data);
-//     };
-//   };
-
-//   return (
-//     <div>
-//       <h1>Image Classification Web App</h1>
-//       <div>
-//         <h3>Take a Picture</h3>
-//         <video autoPlay={true}></video>
-//         <button onClick={takePicture}>Capture</button>
-//       </div>
-//       <div>
-//         <h3>Upload an Image</h3>
-//         <input type="file" accept="image/*" onChange={(e) => uploadImage(e.target.files)} />
-//       </div>
-//       <div>
-//         <h3>Predict</h3>
-//         {image && <img src={image} alt="Selected" width="200" height="200" />}
-//         <button onClick={predict}>Predict</button>
-//         <p>{result}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import * as tf from '@tensorflow/tfjs';
-
-// import './App.css';
-
-// const App = () => {
-//   const [image, setImage] = useState(null);
-//   const [result, setResult] = useState('');
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState(false);
-
-//   const takePicture = async () => {
-//     const mediaDevices = navigator.mediaDevices;
-//     if (mediaDevices && mediaDevices.getUserMedia) {
-//       const stream = await mediaDevices.getUserMedia({ video: true });
-//       const video = document.querySelector('video');
-//       video.srcObject = stream;
-//       video.play();
-//     }
-//   };
-
-//   const uploadImage = (files) => {
-//     const file = files[0];
-//     const reader = new FileReader();
-//     reader.readAsDataURL(file);
-//     reader.onload = async () => {
-//       setImage(reader.result);
-//       const img = new Image();
-//       img.src = reader.result;
-//       img.onload = async () => {
-//         const tensor = tf.browser.fromPixels(img).resizeNearestNeighbor([224, 224]).toFloat();
-//         const offset = tf.scalar(255);
-//         const normalized = tensor.div(offset);
-//         const batched = normalized.expandDims(0);
-//         const formData = new FormData();
-//         formData.append('file', batched.arraySync());
-//         const response = await axios.post('http://localhost:5000/predict', formData);
-//         setResult(response.data);
-//       };
-//     };
-//   };
-
-//   const predict = async () => {
-//     setLoading(true);
-//     setError(false);
-//     setResult(null);
-
-//     setResult('Predicting...');
-//     const img = new Image();
-//     img.src = image;
-//     img.onload = async () => {
-//       const tensor = tf.browser.fromPixels(img).resizeNearestNeighbor([224, 224]).toFloat();
-//       const offset = tf.scalar(255);
-//       const normalized = tensor.div(offset);
-//       const batched = normalized.expandDims(0);
-//       const formData = new FormData();
-//       formData.append("file", image)
-
-//       try {
-//         const response = await axios.post("http://localhost:5000/predict", formData, {
-//           headers: {
-//             "Content-Type": "multipart/form-data",
-//           },
-//         });
-//         setResult(response.data.result);
-//       } catch (error) {
-//         setError(true);
-//       }
-  
-//       setLoading(false);
-
-
-      
-//       // formData.append('image', batched.arraySync());
-//       // const response = await axios.post('http://localhost:5000/predict', formData);
-//       // setResult(response.data);
-//     };
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1 className="App-title">Image Classification Web App</h1>
-//       <div className="App-section">
-//         <h2>Take a Picture</h2>
-//         <div className="App-camera">
-//           <video autoPlay={true}></video>
-//           <button className="App-button" onClick={takePicture}>Capture</button>
-//         </div>
-//       </div>
-//       <div className="App-section">
-//         <h2>Upload an Image</h2>
-//         <div className="App-file">
-//           <input type="file" accept="image/*" onChange={(e) => uploadImage(e.target.files)} />
-//         </div>
-//       </div>
-//       <div className="App-section">
-//         <h2>Predict</h2>
-//         {image && <img src={image} alt="Selected" width="200" height="200" />}
-//         <button className="App-button" onClick={predict}>Predict</button>
-//         <p className="App-result">{result}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
-// import React, { useState } from "react";
-// import axios from "axios";
-// import "./App.css";
-
-// function App() {
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState(false);
-//   const [result, setResult] = useState(null);
-//   const [imageFile, setImageFile] = useState(null);
-
-//   async function handlePredict() {
-//     setLoading(true);
-//     setError(false);
-//     setResult(null);
-
-//     const formData = new FormData();
-//     formData.append("file", imageFile);
-
-//     try {
-//       const response = await axios.post("http://localhost:5000/predict", formData, {
-//         headers: {
-//           "Content-Type": "multipart/form-data",
-//         },
-//       });
-//       setResult(response.data.result);
-//     } catch (error) {
-//       setError(true);
-//     }
-
-//     setLoading(false);
-//   }
-
-//   function handleFileUpload(event) {
-//     const file = event.target.files[0];
-//     setImageFile(file);
-//   }
-
-//   return (
-//     <div className="App">
-//       <h1>Image Classifier</h1>
-//       <div className="Upload">
-//         <input type="file" onChange={handleFileUpload} />
-//         <button onClick={handlePredict}>Predict</button>
-//       </div>
-//       {loading && <p>Loading...</p>}
-//       {error && <p>Error occurred.</p>}
-//       {result && (
-//         <div className="Result">
-//           <img src={URL.createObjectURL(imageFile)} alt="uploaded" />
-//           <p>Predicted class: {result}</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import axios from "axios";
 
 const App = () => {
   const [image, setImage] = useState(null);
   const [prediction, setPrediction] = useState("");
+  const [cameraError, setCameraError] = useState(false);
+  const videoRef = useRef(null);
 
   const handleImageChange = (event) => {
     setImage(event.target.files[0]);
   };
 
+  // Upload images from desktop/files
   const handleUploadClick = () => {
     const fileInput = document.getElementById("imageInput");
     fileInput.click();
+  };
+
+  // Enable webcam preview for taking pic
+  const handleCameraClick = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      videoRef.current.srcObject = stream;
+      setCameraError(false);
+    } catch (error) {
+      console.error(error);
+      setCameraError(true);
+    }
+  };
+
+  // Capture the pic from webcam
+  const handleCaptureClick = () => {
+    if (!videoRef.current.srcObject) {
+      setCameraError(true);
+      return;
+    }
+    const canvas = document.createElement("canvas");
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+    const context = canvas.getContext("2d");
+    context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+    canvas.toBlob((blob) => {
+      setImage(blob);
+    }, "image/jpeg", 0.5);
+    videoRef.current.srcObject.getTracks().forEach((track) => track.stop());
   };
 
   const handlePredict = async () => {
@@ -269,7 +51,8 @@ const App = () => {
     formData.append("file", image);
 
     try {
-      const response = await axios.post("http://localhost:5000/predict", formData);
+      // const response = await axios.post("http://localhost:5000/predict", formData);
+      const response = await axios.post("http://192.168.18.25:5000/predict", formData);
       console.log(response.data);
       setPrediction(response.data.prediction);
       console.log(response.data.prediction);
@@ -280,73 +63,30 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Image Classification App</h1>
+      <h1>Pepero Image Classification App</h1>
       <div>
         <input id="imageInput" type="file" onChange={handleImageChange} hidden />
         <button onClick={handleUploadClick}>Upload Image</button>
+        <button onClick={handleCameraClick}>Take Picture</button>
+        <button onClick={handleCaptureClick}>Capture</button>
       </div>
-      {image && (
-        <div>
-          <img src={URL.createObjectURL(image)} alt="Uploaded" />
-          <button onClick={handlePredict}>Predict</button>
-        </div>
-      )}
+      <div>
+        {image ? (
+          <div>
+            <img src={URL.createObjectURL(image)} alt="Uploaded" style={{ width: "400px", height: "400px" }} />
+            <br></br>
+            <button onClick={handlePredict}>Predict</button>
+          </div>
+        ) : (
+          <div>
+            <video ref={videoRef} style={{ width: "65%" }} autoPlay playsInline></video>
+            {cameraError && <p>Error: Unable to access camera.</p>}
+          </div>
+        )}
+      </div>
       {prediction && <h2>Prediction: {prediction}</h2>}
     </div>
   );
 };
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from "react";
-// import axios from "axios";
-
-// function App() {
-//   const [file, setFile] = useState(null);
-//   const [prediction, setPrediction] = useState("");
-
-//   const handleFileInputChange = (event) => {
-//     setFile(event.target.files[0]);
-//   };
-
-//   const handlePredict = () => {
-//     const formData = new FormData();
-//     formData.append("file", file);
-//     axios
-//       .post("http://localhost:5000/predict", formData)
-//       .then((response) => {
-//         //console.log(response.data.prediction);
-//         console.log(response.data);
-//         setPrediction(response.data.prediction);
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   };
-
-//   return (
-//     <div>
-//       <h1>Image Classifier</h1>
-//       <input type="file" onChange={handleFileInputChange} />
-//       <button onClick={handlePredict}>Predict</button>
-//       {prediction && prediction !== "" ? (
-//         <p>Predicted class: {prediction}</p>
-//       ) : (
-//         <p>No prediction yet</p>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
