e624f02082f73c1e48db466c033709b11a7da30a
KH <deinvictus98@gmail.com>
2023-05-01 17:19:13 +0800

    Tidying code

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,3 @@
-# pip install flask pip install flask_cors pip install tensorflow
-
 from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
@@ -21,12 +19,16 @@ class_names = ['Almond', 'Snowy', 'Crunchy', 'Peanut']
 def preprocess_image(image):
     # Convert the image to RGB mode
     image = image.convert('RGB')
+    
     # Resize the image to the expected input shape of the model
     image = image.resize((224, 224))
+    
     # Convert the image to a numpy array
     image_array = np.array(image)
+    
     # Reshape the image array to have rank 4 (batch of 1)
     image_array = np.expand_dims(image_array, axis=0)
+    
     # Preprocess the image data (normalize and scale)
     image_array = tf.keras.applications.mobilenet_v2.preprocess_input(image_array)
     return image_array
@@ -36,21 +38,28 @@ def preprocess_image(image):
 def predict():
     # Get the uploaded file from the request
     file = request.files['file']
+    
     # Read the file contents and convert to a PIL Image object
     image_bytes = io.BytesIO(file.read())
     image = Image.open(image_bytes)
+    
     # Preprocess the image
     image_array = preprocess_image(image)
+    
     # Use the model to make a prediction
     predictions = model.predict(image_array)
+    
     # Get the predicted class
     predicted_class_index = np.argmax(predictions, axis=1)[0]
     predicted_class = class_names[predicted_class_index]
+    
     # Return the result as a JSON response
     result = {'class': predicted_class}
     print(result)
+    
     #return predicted_class
     # return jsonify(result)
+    
     # Return the prediction as a string
     return jsonify({'prediction': predicted_class})
     
